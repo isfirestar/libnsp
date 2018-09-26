c9457382ea44221cb486878556fe22d591ade756
zhuoyunzhi <250603123@qq.com>
2018-09-26 16:24:41 +0800

    object模块大规模更新

--- com/object.c
+++ com/object.c
@@ -10,170 +10,216 @@
 #endif
 
 #include "object.h"
+#include "clist.h"
 
-#define OBJ_HASHTABLE_SIZE   (399)
+#define OBJ_HASHTABLE_SIZE   (599)
 
 #define OBJSTAT_NORMAL    (0)
 #define OBJSTAT_CLOSEWAIT   (1)
 
-typedef struct _baseobject {
+#if _WIN32 /* WIN32 */
+
+typedef CRITICAL_SECTION MUTEX_T;
+
+#define LOCK    EnterCriticalSection
+#define UNLOCK  LeaveCriticalSection
+
+#define INCREASEMENT(n)    InterlockedIncrement(n)
+
+static void mutex_init(MUTEX_T *mutex)
+{
+    if (mutex) {
+        InitializeCriticalSection(mutex);
+    }
+}
+
+static void mutex_uninit(MUTEX_T *mutex)
+{
+    if (mutex) {
+        DeleteCriticalSection(mutex);
+    }
+}
+
+#else /* POSIX */
+
+typedef pthread_mutex_t MUTEX_T;
+
+#define LOCK    pthread_mutex_lock
+#define UNLOCK  pthread_mutex_unlock
+
+#define INCREASEMENT(n)    __sync_add_and_fetch(n, 1)
+
+static void mutex_init(MUTEX_T *mutex)
+{
+    pthread_mutexattr_t attr;
+    if (mutex) {
+        pthread_mutexattr_init(&attr);
+        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
+        pthread_mutex_init(mutex, &attr);
+    }
+}
+
+static void mutex_uninit(MUTEX_T *mutex)
+{
+    if (mutex) {
+        pthread_mutex_destroy(mutex);
+    }
+}
+
+#endif
+
+typedef struct _object_t {
     objhld_t hld_;
     int stat_;
     int refcnt_;
     int objsizecb_;
     int user_size_;
-    struct _baseobject *next_hash_clash_;
+    struct list_head hash_clash_;
     objinitfn_t initializer_;
     objuninitfn_t unloader_;
-    char user_data_[0];
+    unsigned char user_data_[0];
 } object_t;
 
-#if _WIN32
-
-static
-struct {
-    object_t *object_hash_table_[OBJ_HASHTABLE_SIZE];
+struct _object_manager {
+    struct list_head table_odd_[OBJ_HASHTABLE_SIZE];
+    struct list_head table_even_[OBJ_HASHTABLE_SIZE];
     objhld_t automatic_id_;
-    CRITICAL_SECTION locker_;
-} g_objmgr;
-
-#define LOCK    EnterCriticalSection
-#define UNLOCK  LeaveCriticalSection
-
-#else
-
-static
-struct {
-    object_t *object_hash_table_[OBJ_HASHTABLE_SIZE];
-    objhld_t automatic_id_;
-    pthread_mutex_t locker_;
-} g_objmgr = {
-    { NULL}, 0, PTHREAD_MUTEX_INITIALIZER
+    MUTEX_T object_locker_;
 };
 
-#define LOCK    pthread_mutex_lock
-#define UNLOCK  pthread_mutex_unlock
-
+#if _WIN32 /* WIN32 */
+static struct _object_manager g_objmgr;
+#else
+static struct _object_manager g_objmgr = {
+    { NULL, NULL }, { NULL, NULL }, 0, PTHREAD_MUTEX_INITIALIZER,
+};
 #endif
 
-#define HLD_ROOT(hld)    ((object_t *)(g_objmgr.object_hash_table_[hld % OBJ_HASHTABLE_SIZE]))
+static
+struct list_head *hld2root(objhld_t hld) 
+{
+    struct list_head *root;
+    int idx;
+
+    idx = hld % OBJ_HASHTABLE_SIZE;
+    root = (((hld % 2) == 0) ? &g_objmgr.table_odd_[idx] : &g_objmgr.table_even_[idx]);
+    if (!root->next || !root->prev) {
+        INIT_LIST_HEAD(root);
+    }
+
+    return root;
+}
 
 static
-objhld_t objtabinst(object_t *obj) {
-    object_t * target;
+objhld_t objtabinst(object_t *obj) 
+{
+    struct list_head *root;
 
-    if (!obj || obj->next_hash_clash_) {
-        return -1;
-    }
+    assert(obj);
 
-    LOCK(&g_objmgr.locker_);
+    LOCK(&g_objmgr.object_locker_);
+
+    /* automatic increase handle number */
     obj->hld_ = ++g_objmgr.automatic_id_;
-    target = HLD_ROOT(obj->hld_);
-    if (!target) {
-        g_objmgr.object_hash_table_[obj->hld_ % OBJ_HASHTABLE_SIZE] = obj;
-    } else {
-        while (target->next_hash_clash_) {
-            target = target->next_hash_clash_;
-        }
-        target->next_hash_clash_ = obj;
-    }
-    UNLOCK(&g_objmgr.locker_);
+
+    /* map root pointer from table */
+    root = hld2root(obj->hld_);
+    assert(root);
+
+    /* insert into hash list */
+    list_add_tail(&obj->hash_clash_, root);
+
+    UNLOCK(&g_objmgr.object_locker_);
     return obj->hld_;
 }
 
 static
-void objtabrmve(object_t *obj) {
-	object_t *cursor, *target;
+int objtabrmve(objhld_t hld, object_t **removed) 
+{
+	object_t *target, *cursor;
+    struct list_head *root, *pos, *n;
 
-    if (!obj) {
-        return;
+    target = NULL;
+
+    do {
+        root = hld2root(hld);
+        assert(root);
+
+        list_for_each_safe(pos, n, root) {
+            cursor = containing_record(pos, object_t, hash_clash_);
+            assert(cursor);
+            if (cursor->hld_ == hld) {
+                target = cursor;
+                list_del(pos);
+                INIT_LIST_HEAD(pos);
+                break;
+            }
+        }
+    } while ( 0 );
+
+    if (removed) {
+        *removed = target;
     }
 
-	cursor = NULL;
-	target = NULL;
-
-	do {
-		LOCK(&g_objmgr.locker_);
-		cursor = HLD_ROOT(obj->hld_);
-		if ( !cursor ) {
-			break;
-		}
-
-		if ( cursor == obj ) {
-			 g_objmgr.object_hash_table_[obj->hld_ % OBJ_HASHTABLE_SIZE] = obj->next_hash_clash_;
-			 target = obj;
-			 break;
-		}
-
-		while ( cursor->next_hash_clash_ ) {
-			if ( cursor->next_hash_clash_ == obj ) {
-				cursor->next_hash_clash_ = obj->next_hash_clash_;
-				target = obj;
-				break;
-			}
-			cursor = cursor->next_hash_clash_;
-		}
-	} while ( 0 );
-            
-    UNLOCK(&g_objmgr.locker_);
-
-	/* we must ensure that the deleted target is in the management sequence
-		 otherwise, @obj maybe a wildpointer, @free operation may take application crash.
-		 in this exception case,we will throw a assert fail*/
-	assert( target );
-	if ( target ) {
-		if ( target->unloader_ ) {
-			target->unloader_( target->hld_, target->user_data_ );
-		}
-		free( target );
-	}
+    return ((NULL == target) ? (-1) : (0));
 }
 
 static
-object_t *objtabsrch(const objhld_t hld) {
-    object_t *loop, *found;
+object_t *objtabsrch(const objhld_t hld) 
+{
+    object_t *target, *cursor;
+    struct list_head *root, *pos, *n;
 
-    loop = found = NULL;
+    target = NULL;
 
-    loop = HLD_ROOT(hld);
-    while (loop) {
-        if (loop->hld_ == hld) {
-            found = loop;
-            break;
+    do {
+        root = hld2root(hld);
+        assert(root);
+
+        list_for_each_safe(pos, n, root) {
+            cursor = containing_record(pos, object_t, hash_clash_);
+            assert(cursor);
+            if (cursor->hld_ == hld) {
+                target = cursor;
+                break;
+            }
         }
-        loop = loop->next_hash_clash_;
-    }
+    }while(0);
 
-    return found;
+    return target;
 }
 
-void objinit() {
+static
+void objtagfree(object_t *taget)
+{
+    /* release the object context and free target memory when object removed from table
+        call the unload routine if not null */
+    if ( taget ) {
+        if ( taget->unloader_ ) {
+            taget->unloader_( taget->hld_, (const void *)taget->user_data_ );
+        }
+        free( taget );
+    }
+}
+
+void objinit() 
+{
     static long inited = 0;
-#if _WIN32
-    if (1 == InterlockedIncrement(&inited)) {
-        memset(g_objmgr.object_hash_table_, 0, sizeof ( g_objmgr.object_hash_table_));
+    if ( 1 == INCREASEMENT(&inited)) {
+        memset(g_objmgr.table_odd_, 0, sizeof ( g_objmgr.table_odd_));
+        memset(g_objmgr.table_even_, 0, sizeof ( g_objmgr.table_even_));
         g_objmgr.automatic_id_ = 0;
-        InitializeCriticalSection(&g_objmgr.locker_);
+        mutex_init(&g_objmgr.object_locker_);
     }
-#else
-    pthread_mutexattr_t attr;
-    if (1 == __sync_add_and_fetch(&inited, 1)) {
-        pthread_mutexattr_init(&attr);
-        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
-        pthread_mutex_init(&g_objmgr.locker_, &attr);
-    }
-#endif
 }
 
-void objuninit() {
-#if _WIN32
-    DeleteCriticalSection(&g_objmgr.locker_);
-#else
-    pthread_mutex_destroy(&g_objmgr.locker_);
-#endif
+void objuninit()
+{
+    mutex_uninit(&g_objmgr.object_locker_);
 }
 
-objhld_t objallo(int user_size, objinitfn_t initializer, objuninitfn_t unloader, void *initctx, unsigned int cbctx) {
+objhld_t objallo(int user_size, objinitfn_t initializer, objuninitfn_t unloader, void *initctx, unsigned int cbctx) 
+{
     object_t *obj;
 
     if (0 == user_size) {
@@ -189,14 +235,14 @@ objhld_t objallo(int user_size, objinitfn_t initializer, objuninitfn_t unloader,
     obj->refcnt_ = 0;
     obj->objsizecb_ = user_size + sizeof ( object_t);
     obj->user_size_ = user_size;
-    obj->next_hash_clash_ = NULL;
+    INIT_LIST_HEAD(&obj->hash_clash_);
     obj->initializer_ = initializer;
     obj->unloader_ = unloader;
     memset(obj->user_data_, 0, obj->user_size_);
 
     if (obj->initializer_) {
-        if (obj->initializer_(obj->user_data_, initctx, cbctx) < 0) {
-            obj->unloader_(-1, obj->user_data_);
+        if (obj->initializer_((const void *)obj->user_data_, initctx, cbctx) < 0) {
+            obj->unloader_(-1, (const void *)obj->user_data_);
             free(obj);
             return -1;
         }
@@ -205,74 +251,77 @@ objhld_t objallo(int user_size, objinitfn_t initializer, objuninitfn_t unloader,
     return objtabinst(obj);
 }
 
-void *objrefr(objhld_t hld) {
-    object_t *obj = NULL;
-    char *user_data = NULL;
+const void *objrefr(objhld_t hld) 
+{
+    object_t *obj;
+    unsigned char *user_data;
 
-    LOCK(&g_objmgr.locker_);
+    obj = NULL;
+    user_data = NULL;
+
+    LOCK(&g_objmgr.object_locker_);
     obj = objtabsrch(hld);
     if (obj) {
 		/* object status CLOSE_WAIT will be ignore for @objrefr operation */
         if (OBJSTAT_NORMAL == obj->stat_) {
-            obj->refcnt_++;
-            user_data = (char *) obj->user_data_;
+            ++obj->refcnt_;
+            user_data = obj->user_data_;
         }
     }
-    UNLOCK(&g_objmgr.locker_);
-    return user_data;
+    UNLOCK(&g_objmgr.object_locker_);
+
+    return (const void *)user_data;
 }
 
-void objdefr(objhld_t hld) {
-    object_t *obj = NULL;
-    int rmv = 0;
+void objdefr(objhld_t hld) 
+{
+    object_t *obj, *removed;
 
-    LOCK(&g_objmgr.locker_);
+    obj = NULL;
+    removed = NULL;
+
+    LOCK(&g_objmgr.object_locker_);
     obj = objtabsrch(hld);
     if (obj) {
-		/* in normal, ref count must be greater than zero.
-			otherwise, we will throw a assert fail*/
+		/* in normal, ref-count must be greater than zero. otherwise, we will throw a assert fail*/
 		assert( obj->refcnt_ > 0 );
 		if (obj->refcnt_ > 0 ) {
-           /* if this object is waitting for close and ref count decrease equal to zero, 
+
+            /* decrease the ref-count */
+            --obj->refcnt_;
+
+           /* if this object is waitting for close and ref-count decrease equal to zero, 
 				close it */
-			if ( ( 0 == --obj->refcnt_ ) && ( OBJSTAT_CLOSEWAIT == obj->stat_ ) ) {
-				rmv = 1;
+			if ( ( 0 == obj->refcnt_ ) && ( OBJSTAT_CLOSEWAIT == obj->stat_ ) ) {
+                objtabrmve(obj->hld_, &removed);
 			}
         }
     }
-    UNLOCK(&g_objmgr.locker_);
+    UNLOCK(&g_objmgr.object_locker_);
 
-    if (rmv && obj) {
-        objtabrmve(obj);
-    }
+    removed ? objtagfree(removed) : 0;
 }
 
-void objclos(objhld_t hld) {
-    object_t *obj = NULL;
-    int rmv = 0;
+void objclos(objhld_t hld) 
+{
+    object_t *obj, *removed;
 
-    LOCK(&g_objmgr.locker_);
+    obj = NULL;
+    removed = NULL;
+
+    LOCK(&g_objmgr.object_locker_);
     obj = objtabsrch(hld);
     if (obj) {
-		/* this object is already in CLOSE_WAIT status , 
-			maybe trying an "double close" operation, do nothing*/
-		if ( OBJSTAT_CLOSEWAIT == obj->stat_ ) {
-			;
-		} else {
+        /* if this object is already in CLOSE_WAIT status, maybe trying an "double close" operation, do nothing.
+           if ref-count large than zero, do nothing during this close operation, actual close will take place when the last count dereference. 
+           if ref-count equal to zero, close canbe finish immediately */
+        if ((0 == obj->refcnt_) && (OBJSTAT_NORMAL == obj->stat_)){
+            objtabrmve(obj->hld_, &removed);
+        } else {
             obj->stat_ = OBJSTAT_CLOSEWAIT;
-
-            /* if ref-count large than zero, do nothing during this close operation,
-                actual close will take place when the last count dereference */
-			if ( obj->refcnt_ > 0 ) {
-				;
-			} else {
-				rmv = 1;
-			}
-		}
+        }
     }
-    UNLOCK(&g_objmgr.locker_);
+    UNLOCK(&g_objmgr.object_locker_);
 
-    if (rmv && obj) {
-        objtabrmve(obj);
-    }
+    removed ? objtagfree(removed) : 0;
 }
--- icom/object.h
+++ icom/object.h
@@ -7,20 +7,20 @@ typedef int32_t objhld_t;
 
 #define INVALID_OBJHLD		((objhld_t)(-1))
 
-typedef int( *objinitfn_t)(void *udata, void *ctx, int ctxcb);
-typedef void( *objuninitfn_t)(objhld_t hld, void *udata);
+typedef int( *objinitfn_t)(const void *udata, void *ctx, int ctxcb);
+typedef void( *objuninitfn_t)(objhld_t hld, const void *udata);
 
-__extern__
+extern
 void objinit();
-__extern__
+extern
 void objuninit();
-__extern__
+extern
 objhld_t objallo(int user_data_size, objinitfn_t initializer, objuninitfn_t unloader, void *initctx, unsigned int cbctx);
-__extern__
-void *objrefr(objhld_t hld);
-__extern__
+extern
+const void *objrefr(objhld_t hld);
+extern
 void objdefr(objhld_t hld);
-__extern__
+extern
 void objclos(objhld_t hld);
 
 #endif
