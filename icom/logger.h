4ef8de840d65472e5c9669350bfcac77bc942146
isfirestar <250603123@qq.com>
2020-07-09 17:28:27 +0800

    handle the win32 compiler conflict

--- icom/logger.h
+++ icom/logger.h
@@ -34,9 +34,8 @@ __interface__ void log__flush();
 /* Maximum allowable single log write data length  */
 #define  MAXIMUM_LOG_BUFFER_SIZE  (2048)
 
-#define ECHO(module, fmt, arg...) log__save(module, kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, fmt, ##arg)
-#define ALERT(module, fmt, arg...) log__save(module, kLogLevel_Warning, kLogTarget_Stdout | kLogTarget_Filesystem, fmt, ##arg)
-#define ERROR(module, fmt, arg...) log__save(module, kLogLevel_Error, kLogTarget_Stdout | kLogTarget_Filesystem, fmt, ##arg)
-#define FATAL(module, fmt, arg...) log__save(module, kLogLevel_Fatal, kLogTarget_Filesystem, fmt, ##arg)
+#define ECHO(module, fmt, arg, ...) log__save(module, kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, fmt, ##arg)
+#define ALERT(module, fmt, arg, ...) log__save(module, kLogLevel_Warning, kLogTarget_Stdout | kLogTarget_Filesystem, fmt, ##arg)
+#define FATAL(module, fmt, arg, ...) log__save(module, kLogLevel_Error, kLogTarget_Filesystem, fmt, ##arg)
 
 #endif
