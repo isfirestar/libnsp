cc982a3b6021b7b6cfc82448f4c5f859e63fba19
zhuoyz <250603123@qq.com>
2020-08-25 22:38:22 +0800

    update object management implementations,add function of objallo3, allow user to try a knowned handle-id to insert into memory object pool

--- com/object.c
+++ com/object.c
@@ -25,7 +25,8 @@ typedef CRITICAL_SECTION MUTEX_T;
 #define LOCK    EnterCriticalSection
 #define UNLOCK  LeaveCriticalSection
 
-#define INCREASEMENT(n)    InterlockedIncrement(n)
+#define INCREMENT(n)    InterlockedIncrement(n)
+#define DECREMENT(n)     InterlockedDecrement(n)
 
 static void mutex_init(MUTEX_T *mutex)
 {
@@ -48,7 +49,8 @@ typedef pthread_mutex_t MUTEX_T;
 #define LOCK    pthread_mutex_lock
 #define UNLOCK  pthread_mutex_unlock
 
-#define INCREASEMENT(n)    __sync_add_and_fetch(n, 1)
+#define INCREMENT(n)    __sync_add_and_fetch(n, 1)
+#define DECREMENT(n)    __sync_sub_and_fetch(n, 1)
 
 static void mutex_init(MUTEX_T *mutex)
 {
@@ -70,21 +72,20 @@ static void mutex_uninit(MUTEX_T *mutex)
 #endif
 
 typedef struct _object_t {
-    struct avltree_node_t hash_clash_;
-    objhld_t hld_;
-    int stat_;
-    int refcnt_;
-    int objsizecb_;
-    int user_size_;
-    objinitfn_t initializer_;
-    objuninitfn_t unloader_;
-    unsigned char user_data_[0];
+    struct avltree_node_t clash;    /* the hash conflict item container */
+    objhld_t hld;
+    int status;
+    int refcnt;
+    int size;
+    objinitfn_t initializer;
+    objuninitfn_t unloader;
+    unsigned char body[0];
 } object_t;
 
 struct _object_manager {
-    struct avltree_node_t *object_table_[OBJ_HASHTABLE_SIZE];
-    objhld_t automatic_id_;
-    MUTEX_T object_locker_;
+    struct avltree_node_t *roottab[OBJ_HASHTABLE_SIZE];
+    objhld_t atoid;
+    MUTEX_T mutex;
 };
 
 static int avl_compare_routine(const void *left, const void *right)
@@ -93,14 +94,14 @@ static int avl_compare_routine(const void *left, const void *right)
 
     assert(left && right);
 
-    lobj = containing_record(left, object_t, hash_clash_);
-    robj = containing_record(right, object_t, hash_clash_);
+    lobj = containing_record(left, object_t, clash);
+    robj = containing_record(right, object_t, clash);
 
-    if (lobj->hld_ > robj->hld_) {
+    if (lobj->hld > robj->hld) {
         return 1;
     }
 
-    if (lobj->hld_ < robj->hld_) {
+    if (lobj->hld < robj->hld) {
         return -1;
     }
 
@@ -111,7 +112,7 @@ static int avl_compare_routine(const void *left, const void *right)
 static struct _object_manager g_objmgr;
 #else
 static struct _object_manager g_objmgr = {
-    .object_table_ = { NULL }, 0, PTHREAD_MUTEX_INITIALIZER,
+    .roottab = { NULL }, 0, PTHREAD_MUTEX_INITIALIZER,
 };
 #endif
 
@@ -130,42 +131,55 @@ static struct avltree_node_t **__hld2root(objhld_t hld)
         return NULL;
     }
 
-    root = &g_objmgr.object_table_[idx];
+    root = &g_objmgr.roottab[idx];
     return root;
 }
 
 static objhld_t __objtabinst(object_t *obj)
 {
-    objhld_t hld;
-    struct avltree_node_t **root;
+    object_t find;
+    struct avltree_node_t **root, *target;
 
     if (!obj) {
         return INVALID_OBJHLD;
     }
 
-    /* initial/reinitial the object's handle to INVALID */
-    obj->hld_ = INVALID_OBJHLD;
+    root = NULL;
 
-    LOCK(&g_objmgr.object_locker_);
+    LOCK(&g_objmgr.mutex);
 
     do {
-        /* automatic increase handle number */
-        hld = ++g_objmgr.automatic_id_;
+        /* the creator acquire a knowned handle-id */
+        if (INVALID_OBJHLD != obj->hld) {
+            /* map root pointer from table */
+            root = __hld2root(obj->hld);
+            if (!root) {
+                break;
+            }
 
-        /* map root pointer from table */
-        root = __hld2root(hld);
-        if (!root) {
-            --g_objmgr.automatic_id_;
-            break;
+            find.hld = obj->hld;
+            target = avlsearch(*root, &find.clash, &avl_compare_routine);
+            if (target) {
+                obj->hld = INVALID_OBJHLD;
+                break;
+            }
+        } else {
+            /* automatic increase handle number */
+            obj->hld = ++g_objmgr.atoid;
+            root = __hld2root(obj->hld);
+            if (!root) {
+                break;
+            }
         }
 
+        assert(root);
         /* insert into hash list and using avl-binary-tree to handle the clash */
-        obj->hld_ = hld;
-        *root = avlinsert(*root, &obj->hash_clash_, &avl_compare_routine);
+        *root = avlinsert(*root, &obj->clash, &avl_compare_routine);
     } while (0);
 
-    UNLOCK(&g_objmgr.object_locker_);
-    return obj->hld_;
+    UNLOCK(&g_objmgr.mutex);
+
+    return obj->hld;
 }
 
 static int __objtabrmve(objhld_t hld, object_t **removed)
@@ -180,10 +194,10 @@ static int __objtabrmve(objhld_t hld, object_t **removed)
 
     rmnode = NULL;
 
-    node.hld_ = hld;
-    *root = avlremove(*root, &node.hash_clash_, &rmnode, &avl_compare_routine);
+    node.hld = hld;
+    *root = avlremove(*root, &node.clash, &rmnode, &avl_compare_routine);
     if (rmnode && removed) {
-        *removed = containing_record(rmnode, object_t, hash_clash_);
+        *removed = containing_record(rmnode, object_t, clash);
     }
 
     return ((NULL == rmnode) ? (-1) : (0));
@@ -199,12 +213,12 @@ static object_t *__objtabsrch(const objhld_t hld)
         return NULL;
     }
 
-    node.hld_ = hld;
-    target = avlsearch(*root, &node.hash_clash_, &avl_compare_routine);
+    node.hld = hld;
+    target = avlsearch(*root, &node.clash, &avl_compare_routine);
     if (!target) {
         return NULL;
     }
-    return containing_record(target, object_t, hash_clash_);
+    return containing_record(target, object_t, clash);
 }
 
 static void __objtagfree(object_t *target)
@@ -212,8 +226,8 @@ static void __objtagfree(object_t *target)
     /* release the object context and free target memory when object removed from table
         call the unload routine if not null */
     if ( target ) {
-        if ( target->unloader_ ) {
-            target->unloader_( target->hld_, (void *)target->user_data_ );
+        if ( target->unloader ) {
+            target->unloader( target->hld, (void *)target->body );
         }
         free( target );
     }
@@ -222,47 +236,57 @@ static void __objtagfree(object_t *target)
 PORTABLEIMPL(void) objinit()
 {
     static long inited = 0;
-    if ( 1 == INCREASEMENT(&inited)) {
-        memset(g_objmgr.object_table_, 0, sizeof ( g_objmgr.object_table_));
-        g_objmgr.automatic_id_ = 0;
-        mutex_init(&g_objmgr.object_locker_);
+    if ( 1 == INCREMENT(&inited)) {
+        memset(g_objmgr.roottab, 0, sizeof ( g_objmgr.roottab));
+        g_objmgr.atoid = 0;
+        mutex_init(&g_objmgr.mutex);
+    } else{
+        DECREMENT(&inited);
     }
 }
 
 PORTABLEIMPL(void) objuninit()
 {
-    mutex_uninit(&g_objmgr.object_locker_);
+    mutex_uninit(&g_objmgr.mutex);
 }
 
-PORTABLEIMPL(objhld_t) objallo(int user_size, objinitfn_t initializer, objuninitfn_t unloader, const void *initctx, unsigned int cbctx)
+PORTABLEIMPL(objhld_t)
+objallo3(const struct objcreator *creator)
 {
     object_t *obj;
 
-    if (user_size <= 0) {
+    if (!creator) {
+        return INVALID_OBJHLD;
+    }
+
+    if (creator->size <= 0 || (creator->context && !creator->ctxsize) || (!creator->context && creator->ctxsize) ) {
         return INVALID_OBJHLD;
     }
 
 #if _WIN32
-	objinit();
+    objinit();
 #endif
 
-    obj = (object_t *) malloc(user_size + sizeof(object_t));
+    obj = (object_t *) malloc(creator->size + sizeof(object_t));
     if (!obj) {
         return INVALID_OBJHLD;
     }
 
-    obj->stat_ = OBJSTAT_NORMAL;
-    obj->refcnt_ = 0;
-    obj->objsizecb_ = user_size + sizeof ( object_t);
-    obj->user_size_ = user_size;
-    memset(&obj->hash_clash_, 0, sizeof(obj->hash_clash_));
-    obj->initializer_ = initializer;
-    obj->unloader_ = unloader;
-    memset(obj->user_data_, 0, obj->user_size_);
+    obj->hld = creator->known;
+    if (0 == obj->hld) {
+        obj->hld = INVALID_OBJHLD;  /* treat zero specify to be a invalidate value */
+    }
+    obj->status = OBJSTAT_NORMAL;
+    obj->refcnt = 0;
+    obj->size = creator->size;
+    memset(&obj->clash, 0, sizeof(obj->clash));
+    obj->initializer = creator->initializer;
+    obj->unloader = creator->unloader;
+    memset(obj->body, 0, obj->size);
 
-    if (obj->initializer_) {
-        if (obj->initializer_((void *)obj->user_data_, initctx, cbctx) < 0) {
-            obj->unloader_(-1, (void *)obj->user_data_);
+    if (obj->initializer) {
+        if (obj->initializer((void *)obj->body, creator->context, creator->ctxsize) < 0) {
+            obj->unloader(-1, (void *)obj->body);
             free(obj);
             return -1;
         }
@@ -273,60 +297,101 @@ PORTABLEIMPL(objhld_t) objallo(int user_size, objinitfn_t initializer, objuninit
         return INVALID_OBJHLD;
     }
 
-    return obj->hld_;
+    return obj->hld;
 }
 
-PORTABLEIMPL(objhld_t) objallo2(int user_size)
+PORTABLEIMPL(objhld_t) objallo(int size, objinitfn_t initializer, objuninitfn_t unloader, const void *context, unsigned int ctxsize)
 {
-    return objallo(user_size, NULL, NULL, NULL, 0);
+    struct objcreator creator;
+
+    creator.known = INVALID_OBJHLD;
+    creator.size = size;
+    creator.initializer = initializer;
+    creator.unloader = unloader;
+    creator.context = context;
+    creator.ctxsize = ctxsize;
+    return objallo3(&creator);
+}
+
+PORTABLEIMPL(objhld_t) objallo2(int size)
+{
+    return objallo(size, NULL, NULL, NULL, 0);
 }
 
 PORTABLEIMPL(void *) objrefr(objhld_t hld)
 {
     object_t *obj;
-    unsigned char *user_data;
+    unsigned char *context;
 
     obj = NULL;
-    user_data = NULL;
+    context = NULL;
 
-    LOCK(&g_objmgr.object_locker_);
+    LOCK(&g_objmgr.mutex);
     obj = __objtabsrch(hld);
     if (obj) {
 		/* object status CLOSE_WAIT will be ignore for @objrefr operation */
-        if (OBJSTAT_NORMAL == obj->stat_) {
-            ++obj->refcnt_;
-            user_data = obj->user_data_;
+        if (OBJSTAT_NORMAL == obj->status) {
+            ++obj->refcnt;
+            context = obj->body;
         }
     }
-    UNLOCK(&g_objmgr.object_locker_);
+    UNLOCK(&g_objmgr.mutex);
 
-    return (void *)user_data;
+    return (void *)context;
+}
+
+PORTABLEAPI(int) objrefr2(objhld_t hld, void **output)
+{
+    object_t *obj;
+    int size;
+
+    if (!output) {
+        return -1;
+    }
+
+    obj = NULL;
+    *output = NULL;
+    size = -1;
+
+    LOCK(&g_objmgr.mutex);
+    obj = __objtabsrch(hld);
+    if (obj) {
+        /* object status CLOSE_WAIT will be ignore for @objrefr operation */
+        if (OBJSTAT_NORMAL == obj->status) {
+            ++obj->refcnt;
+            *output = obj->body;
+            size = obj->size;
+        }
+    }
+    UNLOCK(&g_objmgr.mutex);
+
+    return size;
 }
 
 PORTABLEIMPL(void *) objreff(objhld_t hld)
 {
     object_t *obj;
-    unsigned char *user_data;
+    unsigned char *context;
 
     obj = NULL;
-    user_data = NULL;
+    context = NULL;
 
-    LOCK(&g_objmgr.object_locker_);
+    LOCK(&g_objmgr.mutex);
     obj = __objtabsrch(hld);
     if (obj) {
         /* object status CLOSE_WAIT will be ignore for @objrefr operation */
-        if (OBJSTAT_NORMAL == obj->stat_) {
-            ++obj->refcnt_;
-            user_data = obj->user_data_;
+        if (OBJSTAT_NORMAL == obj->status) {
+            ++obj->refcnt;
+            context = obj->body;
 
             /* change the object states to CLOSEWAIT immediately,
                 so, other reference request will fail, object will be close when ref-count decrease equal to zero. */
-            obj->stat_ = OBJSTAT_CLOSEWAIT;
+            obj->status = OBJSTAT_CLOSEWAIT;
         }
     }
-    UNLOCK(&g_objmgr.object_locker_);
+    UNLOCK(&g_objmgr.mutex);
 
-    return (void *)user_data;
+    return (void *)context;
 }
 
 PORTABLEIMPL(void) objdefr(objhld_t hld)
@@ -336,24 +401,24 @@ PORTABLEIMPL(void) objdefr(objhld_t hld)
     obj = NULL;
     removed = NULL;
 
-    LOCK(&g_objmgr.object_locker_);
+    LOCK(&g_objmgr.mutex);
     obj = __objtabsrch(hld);
     if (obj) {
 		/* in normal, ref-count must be greater than zero. otherwise, we will throw a assert fail*/
-		assert( obj->refcnt_ > 0 );
-		if (obj->refcnt_ > 0 ) {
+		assert( obj->refcnt > 0 );
+		if (obj->refcnt > 0 ) {
 
             /* decrease the ref-count */
-            --obj->refcnt_;
+            --obj->refcnt;
 
            /* if this object is waitting for close and ref-count decrease equal to zero,
 				close it */
-			if ( ( 0 == obj->refcnt_ ) && ( OBJSTAT_CLOSEWAIT == obj->stat_ ) ) {
-                __objtabrmve(obj->hld_, &removed);
+			if ( ( 0 == obj->refcnt ) && ( OBJSTAT_CLOSEWAIT == obj->status ) ) {
+                __objtabrmve(obj->hld, &removed);
 			}
         }
     }
-    UNLOCK(&g_objmgr.object_locker_);
+    UNLOCK(&g_objmgr.mutex);
 
     if (removed) {
         __objtagfree(removed);
@@ -366,19 +431,19 @@ PORTABLEIMPL(void) objclos(objhld_t hld)
 
 	removed = NULL;
 
-    LOCK(&g_objmgr.object_locker_);
+    LOCK(&g_objmgr.mutex);
 	obj = __objtabsrch(hld);
     if (obj) {
         /* if this object is already in CLOSE_WAIT status, maybe trying an "double close" operation, do nothing.
            if ref-count large than zero, do nothing during this close operation, actual close will take place when the last count dereference.
            if ref-count equal to zero, close canbe finish immediately */
-        if ((0 == obj->refcnt_) && (OBJSTAT_NORMAL == obj->stat_)){
-            __objtabrmve(obj->hld_, &removed);
+        if ((0 == obj->refcnt) && (OBJSTAT_NORMAL == obj->status)){
+            __objtabrmve(obj->hld, &removed);
         } else {
-            obj->stat_ = OBJSTAT_CLOSEWAIT;
+            obj->status = OBJSTAT_CLOSEWAIT;
         }
     }
-    UNLOCK(&g_objmgr.object_locker_);
+    UNLOCK(&g_objmgr.mutex);
 
     if (removed) {
         __objtagfree(removed);
--- icom/object.h
+++ icom/object.h
@@ -7,17 +7,37 @@ typedef int64_t objhld_t;
 
 #define INVALID_OBJHLD		(~((objhld_t)(0)))
 
-typedef int( *objinitfn_t)(void *udata, const void *ctx, int ctxcb);
-typedef void( *objuninitfn_t)(objhld_t hld, void *udata);
+typedef int( STDCALL * objinitfn_t)(void *udata, const void *ctx, int ctxcb);
+typedef void( STDCALL * objuninitfn_t)(objhld_t hld, void *udata);
 
-PORTABLEAPI(void) objinit(); /* not necessary for Linux/Unix */
-PORTABLEAPI(void) objuninit();	/* object module life cycle tobe the same with process is recommend  */
-PORTABLEAPI(objhld_t) objallo(int user_size, objinitfn_t initializer, objuninitfn_t unloader, const void *initctx, unsigned int cbctx);
-/* simple way to allocate a object, calling thread can use @objreff to final reference and unloaded the object user data segment  */
-PORTABLEAPI(objhld_t) objallo2(int user_size);
-PORTABLEAPI(void *) objrefr(objhld_t hld);	/* object reference */
-PORTABLEAPI(void) objdefr(objhld_t hld);		/* object deference */
-PORTABLEAPI(void *) objreff(objhld_t hld);	/* object reference final */
+struct objcreator
+{
+    /* opt, default to -1, set it to a effective value meat object will use the hanle-id to try insert into memory-pool and failure on existed */
+    objhld_t known;
+    /* MUST large than zero */
+    int size;
+    /* opt,can be null, if specified, this routine will be invoke before object insert into memory-pool */
+    objinitfn_t initializer;
+    /* opt,can be null, if specified, this routine wiil be invoke after object finilly remove from memory-pool */
+    objuninitfn_t unloader;
+    /* opt,can be null, when this field is non-null pointer, @ctxsize field MUST set to the size of bytes of pointer @context pointer to. */
+    const void *context;
+    unsigned int ctxsize;
+};
+
+PORTABLEAPI(void) objinit(); /* not necessary for POSIX */
+PORTABLEAPI(void) objuninit();
+/* obsolete implementation, normal object allocate function, new application should use @objallo3 instead of it */
+PORTABLEAPI(objhld_t) objallo(int size, objinitfn_t initializer, objuninitfn_t unloader, const void *context, unsigned int ctxsize);
+/* obsolete implementation, simple way to allocate a object, new application should use @objallo3 instead of it  */
+PORTABLEAPI(objhld_t) objallo2(int size);
+/* try to allocate a object with a knwon handle-identify specified by @hld,
+    when success, the return value equivalent to input parameter @hld, otherwise INVALID_OBJHLD returned */
+PORTABLEAPI(objhld_t) objallo3(const struct objcreator *creator);
+PORTABLEAPI(void *) objrefr(objhld_t hld);	/* reference object, NULL on failure */
+PORTABLEAPI(int) objrefr2(objhld_t hld, void **output); /* reference object, return the size of object context on success */
+PORTABLEAPI(void) objdefr(objhld_t hld);		/* deference object */
+PORTABLEAPI(void *) objreff(objhld_t hld);	/* object finallize reference, after success call, object status is going to change to CLOSE_WAIT */
 PORTABLEAPI(void) objclos(objhld_t hld);		/* object mark close */
 
 #endif
